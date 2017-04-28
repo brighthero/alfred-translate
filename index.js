678279bdd5983e1336a626efd94d330ff9df6f29
brighthero <marten.hamborg@gmail.com>
2017-04-28 23:30:27 +0200

    Iterate through all langs

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const translate = require('google-translate-api');
 let from = 'auto';
 let to = ['en', 'fr', 'es', 'de', 'zh-cn', 'ja'];
 
+// TODO: Use actual query
+let query = 'Heute ist ein schöner Tag.';
+
 const langParse = /^[a-z]{2,3}(?:-[A-Z]{2,3}(?:-[a-zA-Z]{4})?)?(?::[a-z]{2,3}(?:-[A-Z]{2,3}(?:-[a-zA-Z]{4})?)?)?\s/;
 const langArr = alfy.input.match(langParse);
-let options = {};
 
 if (langArr) {
 	const parseArr = langArr[0].split(':');
@@ -20,16 +22,29 @@ if (langArr) {
 	}
 }
 
-console.log(from, to)
+let output = [];
 
-translate(alfy.input, { to: 'en' }).then(res => {
-
-	alfy.output([
-		{
+to.forEach((val, i) => {
+	translate(query, {
+		from,
+		to: val
+	}).then(res => {
+		// console.log(res);
+		output.push({
 			title: res.text,
-			autocomplete: "Desktop",
-		}
-	]);
-}).catch(err => {
-     console.error(err);
+			subtitle: res.from.text.value,
+			arg: res.text
+		});
+		if (to.length == output.length) done();
+	}).catch(err => {
+    	console.error(err);
+	});
 });
+
+
+function done() {
+	console.log(from, to);
+	console.log(output);
+
+	alfy.output(output);
+}
