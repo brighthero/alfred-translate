245f419f9be988fb2b023415207bfd563f273612
brighthero <marten.hamborg@gmail.com>
2017-04-30 15:31:51 +0200

    Use actual Alfred query

--- index.js
+++ index.js
@@ -7,12 +7,10 @@ const langs = require('./languages.js');
 let from = 'auto';
 let to = ['en', 'fr', 'es', 'de', 'zh-cn', 'ja'];
 
-// TODO: Use actual query
-let query = 'Heute ist ein schöner Tag.';
-
 const langParse = new RegExp("^(" + langs.code.join('|') + ")(?::(" + langs.code.join('|') + "))?\\s");
 const langArr = alfy.input.match(langParse);
 
+// Parse lang selection
 if (langArr) {
 	to = [];
 	const parseArr = langArr[0].split(':');
@@ -26,25 +24,36 @@ if (langArr) {
 
 let output = [];
 
-to.forEach((val, i) => {
-	translate(query, {
-		from,
-		to: val
-	}).then(res => {
-		// console.log(res);
-		output.push({
-			title: res.text,
-			subtitle: res.from.text.value,
-			arg: res.text
+// Parse query input
+const query = alfy.input.replace(langArr && langArr[0] ? langArr[0] : '', '');
+
+if (query !== '') {
+	to.forEach((val, i) => {
+		translate(query, {
+			from,
+			to: val
+		}).then(res => {
+			// console.log(res);
+			output.push({
+				title: res.text,
+				subtitle: res.from.text.value,
+				arg: res.text
+			});
+			if (to.length == output.length) done();
+		}).catch(err => {
+			console.error(err);
 		});
-		if (to.length == output.length) done();
-	}).catch(err => {
-    	console.error(err);
 	});
-});
+} else {
+	output.push({
+		title: 'No text input',
+		subtitle: 'Please enter the text you want to get translated!',
+		arg: ''
+	});
+}
 
 function done() {
-	console.log(from, to);
+	//console.log(from, to);
 	//console.log(output);
 
 	alfy.output(output);
