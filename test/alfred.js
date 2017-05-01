97a0beef80805f12439136a4817cf90d2d6787d7
brighthero <marten.hamborg@gmail.com>
2017-05-01 16:28:08 +0200

    Rename test to lowercase

--- test/alfred.js
+++ test/alfred.js
@@ -1,28 +1,28 @@
 import test from 'ava';
 import alfyTest from 'alfy-test';
 
-test('Translate with defined languages', async t => {
+test('translate with defined languages', async t => {
 	const alfy = alfyTest();
 	const result = await alfy('de:en Alfred kann jetzt übersetzen!');
 
 	t.is(result[0].title, 'Alfred can now translate!');
 });
 
-test('Input language detected', async t => {
+test('input language detected', async t => {
 	const alfy = alfyTest();
 	const result = await alfy('de:en Alfred kann jetzt übersetzen!');
 
 	t.is(result[0].meta.fromlang, 'de');
 });
 
-test('Translated language detected', async t => {
+test('translated language detected', async t => {
 	const alfy = alfyTest();
 	const result = await alfy('de:en Alfred kann jetzt übersetzen!');
 
 	t.is(result[0].meta.tolang, 'en');
 });
 
-test('Auto-detect language', async t => {
+test('auto-detect language', async t => {
 	const alfy = alfyTest();
 	const result = await alfy('Alfred kann jetzt übersetzen!');
 
@@ -31,14 +31,14 @@ test('Auto-detect language', async t => {
 	t.is(result[2].title, 'Alfred ahora puede traducir!');
 });
 
-test('Auto-complete suggestions', async t => {
+test('auto-complete suggestions', async t => {
 	const alfy = alfyTest();
 	const result = await alfy('de Alfred kann jetzt übersetzhhen!');
 
 	t.is(result[0].autocomplete, 'de Alfred kann jetzt übersetzen!');
 });
 
-test('Dont auto-complete suggestions', async t => {
+test('dont auto-complete suggestions', async t => {
 	const alfy = alfyTest();
 	const result = await alfy('de einfach');
 
